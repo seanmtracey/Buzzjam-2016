46c0cbcbb03d0da9204bb79ebc85d815614695cf
seanmtracey <seanmtracey@gmail.com>
2016-09-18 16:32:21 +0100

    Actual pins

--- pi_btn_receiver/index.js
+++ pi_btn_receiver/index.js
@@ -14,7 +14,7 @@ const pinStates = [0,0,0,0,0,0,0,0];
 gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
 
-    if(value === true){
+    if(value === 1){
       socket.emit('buttonPress', {
         key : pins.indexOf(channel)
       });
